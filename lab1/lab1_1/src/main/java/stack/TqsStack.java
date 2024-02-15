985d8aebec3677a8addabdb8c7a5da6eb4ac1e1e
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-02-15 19:16:10 +0000

    (lab1) refactor: Created interface Stack

--- lab1/lab1_1/src/main/java/stack/TqsStack.java
+++ lab1/lab1_1/src/main/java/stack/TqsStack.java
@@ -2,7 +2,7 @@ package stack;
 
 import java.util.LinkedList;
 
-public class TqsStack<T> {
+public class TqsStack<T> implements Stack<T> {
     private static final String NOT_IMPLEMENTED = "Not implemented yet";
 
     private LinkedList<T> list = new LinkedList<T>();
@@ -11,22 +11,27 @@ public class TqsStack<T> {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }
 
+    @Override
     public T pop() {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }
 
+    @Override
     public void push(T element) {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }
 
+    @Override
     public T peek() {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }
 
+    @Override
     public int size() {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }
 
+    @Override
     public boolean isEmpty() {
         throw new UnsupportedOperationException(NOT_IMPLEMENTED);
     }

e46d7539b4e13d75751d256ca2651361a92c7421
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-02-15 17:53:04 +0000

    feature: Created skeleton of TqsStack class

---
+++ lab1/lab1_1/src/main/java/stack/TqsStack.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.LinkedList;
+
+public class TqsStack<T> {
+    private static final String NOT_IMPLEMENTED = "Not implemented yet";
+
+    private LinkedList<T> list = new LinkedList<T>();
+
+    public TqsStack() {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    public T pop() {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    public void push(T element) {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    public T peek() {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException(NOT_IMPLEMENTED);
+    }
+}

a8a5ed243399187ca540d9fe7438ad7ce2ad3b16
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-02-17 17:47:15 +0000

    (lab1) test: corrected existing tests and created new ones

--- lab1/lab1_2euromillions/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
+++ lab1/lab1_2euromillions/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
@@ -7,7 +7,6 @@ package tqs.sets;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
-import tqs.sets.BoundedSetOfNaturals;
 
 /**
  * @author ico0
@@ -17,40 +16,126 @@ class BoundedSetOfNaturalsTest {
     private BoundedSetOfNaturals setB;
     private BoundedSetOfNaturals setC;
 
-
     @BeforeEach
-    public void setUp() {
-        setA = new BoundedSetOfNaturals(1);
-        setB = BoundedSetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
-        setC = BoundedSetOfNaturals.fromArray(new int[]{50, 60});
+    void setUp() {
+        setA = new BoundedSetOfNaturals(2);
+        setB = BoundedSetOfNaturals.fromArray(new int[] { 10, 20, 30, 40, 50, 60 });
+        setC = BoundedSetOfNaturals.fromArray(new int[] { 50, 60 });
     }
 
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
         setA = setB = setC = null;
     }
 
-    @Disabled("TODO revise test logic")
+    @DisplayName("Add element to set")
     @Test
-    public void testAddElement() {
+    void testAddElement() {
 
         setA.add(99);
         assertTrue(setA.contains(99), "add: added element not found in set.");
         assertEquals(1, setA.size());
-
-        setB.add(11);
-        assertTrue(setB.contains(11), "add: added element not found in set.");
-        assertEquals(7, setB.size(), "add: elements count not as expected.");
     }
 
-    @Disabled("TODO revise to test the construction from invalid arrays")
+    @DisplayName("Set refuseses invalid inputs using FromArray method")
     @Test
-    public void testAddFromBadArray() {
-        int[] elems = new int[]{10, -20, -30};
+    void testAddFromBadArray() {
+        int[] elems = new int[] { 10, -20, -30 };
 
         // must fail with exception
         assertThrows(IllegalArgumentException.class, () -> setA.add(elems));
     }
 
+    @DisplayName("Set refuses duplicate inputs")
+    @Test
+    void testAddDuplicateInput() {
+        int num1 = 10;
+        setA.add(num1);
+        assertThrows(IllegalArgumentException.class, () -> {
+            setA.add(num1);
+        }, "add: element was added when it was already in set.");
+    }
 
+    @DisplayName("Set refuses illegal number")
+    @Test
+    void testAddIllegalNumber() {
+        int bad_num = -20;
+        assertThrows(IllegalArgumentException.class, () -> {
+            setA.add(bad_num);
+        }, "add: element was added when it is a illegal number.");
+    }
+
+    @DisplayName("Set refuses addition when at max size")
+    @Test
+    void testAddWhenAtMaxSize() {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        setA.add(num1);
+        setA.add(num2);
+        assertThrows(IllegalArgumentException.class, () -> {
+            setA.add(num3);
+        }, "add: element was added when set was at max size.");
+    }
+
+    @DisplayName("Set is not equal to null")
+    @Test
+    void testEqualsNull() {
+        assertFalse(setA.equals(null), "equals: setA is not null");
+    }
+
+    @DisplayName("Set is not equal to a different type")
+    @Test
+    void testEqualsDifferentType() {
+        assertFalse(setA.equals(2), "equals: setA != 2");
+    }
+
+    @DisplayName("Set is equal to itself")
+    @Test
+    void testEqualsSelf() {
+        assertEquals(setA, setA, "equals: setA must equal setA");
+    }
+
+    @DisplayName("Hashcode must be the same when calculated for the same object several times")
+    @Test
+    void testHashCodeConsistency() {
+        int hashCode = setA.hashCode();
+
+        for (int i = 0; i < 10; i++) {
+            assertEquals(hashCode, setA.hashCode());
+        }
+    }
+
+    @DisplayName("Hashcode must be the same for two objects who are equal")
+    @Test
+    void testHashCodeEquality() {
+        BoundedSetOfNaturals set = new BoundedSetOfNaturals(5);
+        BoundedSetOfNaturals anotherset = new BoundedSetOfNaturals(5);
+
+        assertEquals(set, anotherset);
+
+        assertEquals(set.hashCode(), anotherset.hashCode());
+    }
+
+    @DisplayName("Hashcode must be different for two objects who are not equal")
+    @Test
+    void testHashCodeInequality() {
+        assertNotEquals(setA, setB);
+        assertNotEquals(setB, setC);
+
+        assertNotEquals(setA.hashCode(), setB.hashCode());
+        assertNotEquals(setB.hashCode(), setC.hashCode());
+    }
+
+    @DisplayName("Intersect returns true if a subset intersects with the main set")
+    @Test
+    void testIntersect() {
+        BoundedSetOfNaturals setD = BoundedSetOfNaturals.fromArray(new int[] { 50, 60, 70 });
+
+        assertTrue(setB.intersects(setC));
+        assertTrue(setC.intersects(setB));
+
+        assertFalse(setB.intersects(setD));
+        assertFalse(setD.intersects(setB));
+    }
 }

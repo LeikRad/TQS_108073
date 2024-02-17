3181311873e3894b12921907f3c0aa3ec53bbc57
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-02-17 16:34:40 +0000

    (lab1) style: formatted test

--- lab1/lab1_2euromillions/src/test/java/tqs/euromillions/DipTest.java
+++ lab1/lab1_2euromillions/src/test/java/tqs/euromillions/DipTest.java
@@ -16,10 +16,9 @@ public class DipTest {
 
     private Dip sampleInstance;
 
-
     @BeforeEach
     public void setUp() {
-        sampleInstance = new Dip(new int[]{10, 20, 30, 40, 50}, new int[]{1, 2});
+        sampleInstance = new Dip(new int[] { 10, 20, 30, 40, 50 }, new int[] { 1, 2 });
     }
 
     @AfterEach
@@ -40,14 +39,13 @@ public class DipTest {
 
         // insufficient args
         assertThrows(IllegalArgumentException.class,
-                () -> new Dip( new int[]{10, 11}, new int[]{} ) );
+                () -> new Dip(new int[] { 10, 11 }, new int[] {}));
 
-        //negative numbers
+        // negative numbers
         assertThrows(IllegalArgumentException.class,
-                () -> new Dip( new int[]{10, 11, 12, 13, -1}, new int[]{1, 2} ) );
+                () -> new Dip(new int[] { 10, 11, 12, 13, -1 }, new int[] { 1, 2 }));
 
         // this test will reveal that the code was not yet checking ranges
-
 
     }
 
@@ -57,12 +55,10 @@ public class DipTest {
         // creating Dip with numbers or starts outside the expected range
         // expects an exception
         assertThrows(IllegalArgumentException.class,
-                () -> new Dip( new int[]{10, 11, 12, 13, Dip.NUMBERS_RANGE_MAX * 2}, new int[]{1,2} ) );
+                () -> new Dip(new int[] { 10, 11, 12, 13, Dip.NUMBERS_RANGE_MAX * 2 }, new int[] { 1, 2 }));
         assertThrows(IllegalArgumentException.class,
-                () -> new Dip( new int[]{11, 12, 13, 14, 15}, new int[]{ Dip.STARS_RANGE_MAX*2 ,1} ) );
+                () -> new Dip(new int[] { 11, 12, 13, 14, 15 }, new int[] { Dip.STARS_RANGE_MAX * 2, 1 }));
 
     }
-
-
 
 }

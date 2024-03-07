699b79a539b13e1ac23f7d6c367ccd64ca265f06
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-03-07 15:41:19 +0000

    (Class 4) Feat: Refactored for exercise B

---
+++ lab4/lab4_1hello_world/src/test/java/leikrad/dev/HellowWorldChomeJupiterRefactorTest.java
@@ -0,0 +1,30 @@
+package leikrad.dev;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+
+@ExtendWith(SeleniumJupiter.class)
+class HelloWorldChromeJupiterRefactorTest {
+
+    static final Logger log = getLogger(lookup().lookupClass());
+
+    @Test
+    void test_refactor(FirefoxDriver driver) {
+        // Exercise
+        String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(sutUrl);
+        String title = driver.getTitle();
+        log.debug("The title of {} is {}", sutUrl, title);
+
+        // Verify
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
+    }
+}

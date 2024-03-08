cdd9b023c42fd5ce527bc20457f27af77e591fb8
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-03-08 16:53:08 +0000

    (Class 4) Feat: Added test using docker for chrome browser (lab 4.4)

--- lab4/lab4_3page_object/src/test/java/leikrad/dev/BasicPOMTest.java
+++ lab4/lab4_3page_object/src/test/java/leikrad/dev/BasicPOMTest.java
@@ -3,10 +3,13 @@ package leikrad.dev;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.seljup.DockerBrowser;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 
+import static io.github.bonigarcia.seljup.BrowserType.CHROME;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import leikrad.dev.POMs.CitySelectionPage;
@@ -20,13 +23,9 @@ public class BasicPOMTest {
     PurchasePage purchasePage;
     ReservePage reservePage;
 
-    @BeforeEach
-    public void setUp(FirefoxDriver driver) {
-        citySelectionPage = new CitySelectionPage(driver);
-    }
-
     @Test
-    public void test() {
+    public void test(FirefoxDriver driver) {
+        citySelectionPage = new CitySelectionPage(driver);
         reservePage = citySelectionPage.with("Boston", "New York");
         assertThat(citySelectionPage.success());
         purchasePage = reservePage.chooseFlight();
@@ -35,4 +34,17 @@ public class BasicPOMTest {
                 "John Doe");
         assertThat(purchasePage.success());
     }
+
+    @Test
+    public void test_chrome(@DockerBrowser(type = CHROME) WebDriver driver) {
+        citySelectionPage = new CitySelectionPage(driver);
+        reservePage = citySelectionPage.with("Boston", "New York");
+        assertThat(citySelectionPage.success());
+        purchasePage = reservePage.chooseFlight();
+        assertThat(reservePage.success());
+        purchasePage.with("John Doe", "123 Main St", "Boston", "MA", "12345", "Visa", "1234567890", "12", "2023",
+                "John Doe");
+        assertThat(purchasePage.success());
+    }
+
 }

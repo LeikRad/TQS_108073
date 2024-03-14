6ca37a6d17d3d212af096955938d3d92e8b3ff14
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-03-14 16:42:03 +0000

    (Class 5) Feat: Added tests and skeleton Calculator class

---
+++ lab5/lab5_1calculator/src/main/java/leikrad/dev/Calculator.java
@@ -0,0 +1,22 @@
+package leikrad.dev;
+
+import java.util.Stack;
+
+public class Calculator {
+    private Stack<Integer> stack = new Stack<>();
+
+    public Calculator() {
+    }
+
+    public void push(int arg) {
+        stack.push(arg);
+    }
+
+    public void push(String op) {
+
+    }
+
+    public Number value() {
+        return stack.peek();
+    }
+}

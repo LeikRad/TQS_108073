ceb20df118ba76616db419c6960c8c392075bb86
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-04-01 12:55:33 +0100

    (HW) Lombok added

--- HW1/backend/src/main/java/leikrad/dev/homework1/data/reservation/Reservation.java
+++ HW1/backend/src/main/java/leikrad/dev/homework1/data/reservation/Reservation.java
@@ -3,9 +3,11 @@ package leikrad.dev.homework1.data.reservation;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import leikrad.dev.homework1.data.trip.Trip;
+import lombok.*;
 
 @Entity
 @Table(name = "reservations")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @RequiredArgsConstructor
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,75 +16,21 @@ public class Reservation {
     @ManyToOne
     @JoinColumn(name = "trip_id")
     @NotNull
+    @NonNull
     private Trip trip;
 
     @Column(name = "person_name")
     @NotNull
+    @NonNull
     private String personName;
 
     @Column(name = "phone_number")
     @NotNull
+    @NonNull
     private String phoneNumber;
 
     @Column(name = "uuid")
     @NotNull
+    @NonNull
     private String uuid;
-
-    public Reservation() {
-    }
-
-    public Reservation(Trip trip, String personName, String phoneNumber, String uuid) {
-        this.trip = trip;
-        this.personName = personName;
-        this.phoneNumber = phoneNumber;
-        this.uuid = uuid;
-    }
-
-    public Reservation(Long reservationId, Trip trip, String personName, String phoneNumber, String uuid) {
-        this.reservationId = reservationId;
-        this.trip = trip;
-        this.personName = personName;
-        this.phoneNumber = phoneNumber;
-        this.uuid = uuid;
-    }
-
-    public Long getReservationId() {
-        return reservationId;
-    }
-
-    public void setReservationId(Long reservationId) {
-        this.reservationId = reservationId;
-    }
-
-    public Trip getTrip() {
-        return trip;
-    }
-
-    public void setTrip(Trip trip) {
-        this.trip = trip;
-    }
-
-    public String getPersonName() {
-        return personName;
-    }
-
-    public void setPersonName(String personName) {
-        this.personName = personName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
 }

6ec0a0db5679eaee6d51d714684cb14c31942d32
LeikRad <48689668+LeikRad@users.noreply.github.com>
2024-04-09 15:10:34 +0100

    Fix for last test

--- HW1/acceptance_test/src/test/java/leikrad/dev/AcceptanceTest.java
+++ HW1/acceptance_test/src/test/java/leikrad/dev/AcceptanceTest.java
@@ -6,15 +6,21 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static io.github.bonigarcia.seljup.BrowserType.CHROME;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+
+import java.net.URI;
+import java.net.URL;
+
 import io.github.bonigarcia.seljup.DockerBrowser;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import org.openqa.selenium.JavascriptExecutor;
 import java.util.*;
@@ -27,8 +33,16 @@ class AcceptanceTest {
     
     @Test
     void interactiveTest() {
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+        URL url = null;
+        try {
+            URI uri = new URI("http://localhost:4444/wd/hub");
+            url = uri.toURL();
+        } catch (Exception e) {
+            assertThat(false).isTrue();
+        }
+        WebDriver driver = new RemoteWebDriver(url, capabilities);
 
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
